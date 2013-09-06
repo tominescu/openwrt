f1a6c1dfb31f1e95eca394682b5529654f389dd8
tominescu <tominescu@gmail.com>
2013-09-06 17:01:03 +0800

    Add support for Simplified Chinese CP936

--- package/kernel/linux/modules/nls.mk
+++ package/kernel/linux/modules/nls.mk
@@ -100,6 +100,22 @@ endef
 $(eval $(call KernelPackage,nls-cp866))
 
 
+define KernelPackage/nls-cp936
+  SUBMENU:=Native Language Support
+  TITLE:=Codepage 936 (china)
+  KCONFIG:=CONFIG_NLS_CODEPAGE_936
+  FILES:=$(LINUX_DIR)/fs/nls/nls_cp936.ko
+  AUTOLOAD:=$(call AutoLoad,25,nls_cp936)
+  $(call AddDepends/nls)
+endef
+
+define KernelPackage/nls-cp437/description
+Kernel module for NLS Codepage 936 (china)
+endef
+
+$(eval $(call KernelPackage,nls-cp936))
+
+
 define KernelPackage/nls-cp1250
   SUBMENU:=Native Language Support
   TITLE:=Codepage 1250 (Eastern Europe)
